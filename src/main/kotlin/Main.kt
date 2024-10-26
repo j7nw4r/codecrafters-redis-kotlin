fad129489e936144d2d329b04ad07c227d17328d
Johnathan W <j7n.w4r@gmail.com>
2024-10-26 13:20:55 -0400

    Printing Pong

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,16 +1,28 @@
+import java.io.PrintWriter
 import java.net.ServerSocket
+import java.net.Socket
 
+/*
+Run codecrafters-redis-kotlin.
+ */
 fun main(args: Array<String>) {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     System.err.println("Logs from your program will appear here!")
 
-//     Uncomment this block to pass the first stage
-     var serverSocket = ServerSocket(6379)
-    //
+    var serverSocket = ServerSocket(6379)
      // Since the tester restarts your program quite often, setting SO_REUSEADDR
      // ensures that we don't run into 'Address already in use' errors
      serverSocket.reuseAddress = true
 
-     serverSocket.accept() // Wait for connection from client.
-     println("accepted new connection")
+    while (true) {
+        val socket = serverSocket.accept() // Wait for connection from client.
+        handleConnection(socket)
+    }
+}
+
+fun handleConnection(socket: Socket) {
+    println("accepted new connection")
+    PrintWriter(socket.getOutputStream(), false).use {
+        it.print("+PONG\r\n")
+    }
 }
